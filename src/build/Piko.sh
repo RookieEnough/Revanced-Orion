047d4fc0389db83fae98a8324f3468f3b8ccca8b
Sarthak Sinha <128968367+RookieEnough@users.noreply.github.com>
2025-11-22 19:02:52 +0530

    Update Piko.sh

--- src/build/Piko.sh
+++ src/build/Piko.sh
@@ -1,34 +1,16 @@
 #!/bin/bash
-# Twitter Piko - Simplified version
+# Twitter Piko
 source src/build/utils.sh
 
-echo "[+] Starting Twitter Piko build..."
-
-# Download requirements
 dl_gh "revanced-cli" "revanced" "v4.6.0"
 dl_gh "piko revanced-integrations" "crimera" "latest"
 
-# Download Twitter directly from Uptodown (bypass APKMirror completely)
-echo "[+] Downloading Twitter from Uptodown..."
-url="https://twitter.en.uptodown.com/android/download"
-download_url="https://dw.uptodown.com/dwn/$(req "$url" - | $pup -p --charset utf-8 'button#detail-download-button attr{data-url}')"
-req "$download_url" -o "./download/twitter-stable.apk"
-
-if [ ! -f "./download/twitter-stable.apk" ]; then
-    echo "[-] Failed to download Twitter from Uptodown"
-    exit 1
-fi
-
-echo "[+] Twitter APK downloaded successfully"
-
-# Apply patches directly without using get_patches_key (which triggers APKMirror)
-echo "[+] Applying Piko patches..."
-java -jar *cli*.jar patch \
-    -p *.rvp \
-    --merge *integration*.apk \
-    --out=./release/twitter-stable-piko.apk \
-    --keystore=./src/_ks.keystore \
-    --purge=true \
-    ./download/twitter-stable.apk
-
-echo "[+] Twitter Piko build completed successfully!"
+# Patch Twitter Piko:
+get_patches_key "twitter-piko"
+get_apk "com.twitter.android" "twitter-stable" "twitter" "x-corp/twitter/x-formerly-twitter" "Bundle_extract"
+split_editor "twitter-stable" "twitter-stable"
+patch "twitter-stable" "piko"
+# Patch Twitter Piko Arm64-v8a:
+get_patches_key "twitter-piko"
+split_editor "twitter-stable" "twitter-arm64-v8a-stable" "exclude" "plit_config.armeabi_v7a split_config.x86 split_config.x86_64 split_config.mdpi split_config.hdpi split_config.xhdpi split_config.xxhdpi split_config.tvdpi"
+patch "twitter-arm64-v8a-stable" "piko"
