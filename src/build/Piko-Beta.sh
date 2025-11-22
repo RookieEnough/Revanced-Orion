fb68c70aa887d58b98d6cca3959365bde6e29f79
Sarthak Sinha <128968367+RookieEnough@users.noreply.github.com>
2025-11-22 18:27:51 +0530

    Update Piko-Beta.sh

--- src/build/Piko-Beta.sh
+++ src/build/Piko-Beta.sh
@@ -7,10 +7,14 @@ dl_gh "piko" "crimera" "prerelease"
 
 # Patch Twitter Piko:
 get_patches_key "twitter-piko"
-get_apk "com.twitter.android" "twitter-beta" "twitter" "x-corp/twitter/x-formerly-twitter" "Bundle_extract"
+
+# Use the correct APKMirror URL structure for Twitter/X Beta
+get_apk "com.twitter.android" "twitter-beta" "x" "x-corp/twitter/x" "Bundle_extract"
+
 split_editor "twitter-beta" "twitter-beta"
 patch "twitter-beta" "piko"
+
 # Patch Twitter Piko Arm64-v8a:
 get_patches_key "twitter-piko"
-split_editor "twitter-beta" "twitter-arm64-v8a-beta" "exclude" "plit_config.armeabi_v7a split_config.x86 split_config.x86_64 split_config.mdpi split_config.hdpi split_config.xhdpi split_config.xxhdpi split_config.tvdpi"
+split_editor "twitter-beta" "twitter-arm64-v8a-beta" "exclude" "split_config.armeabi_v7a split_config.x86 split_config.x86_64 split_config.mdpi split_config.hdpi split_config.xhdpi split_config.xxhdpi split_config.tvdpi"
 patch "twitter-arm64-v8a-beta" "piko"
