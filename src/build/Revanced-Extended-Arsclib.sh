13a0efd41e56ed72870f70beb1ff78a18f191894
Sarthak Sinha <128968367+RookieEnough@users.noreply.github.com>
2025-12-26 19:46:32 +0530

    Update Revanced-Extended-Arsclib.sh

--- src/build/Revanced-Extended-Arsclib.sh
+++ src/build/Revanced-Extended-Arsclib.sh
@@ -13,14 +13,18 @@ dl_gh "revanced-patches-arsclib revanced-integrations revanced-cli-arsclib" "ino
 get_patches_key "reddit-rve-arsclib"
 get_apk "com.reddit.frontpage" "reddit" "reddit" "redditinc/reddit/reddit" "Bundle_extract"
 split_editor "reddit" "reddit"
+
+# Patching
 patch "reddit" "revanced-extended"
-mv ./release/reddit-revanced-extended.apk/base.apk ./reddit-revanced-extended.apk
-rm -f -d ./release/reddit-revanced-extended.apk
-mv ./reddit-revanced-extended.apk ./release/reddit-revanced-extended-arsclib.apk
+mv "./release/reddit-revanced-extended-v${APP_VERSION}.apk" "./release/reddit-revanced-extended-arsclib-v${APP_VERSION}.apk"
+
+
 # Patch Arm64-v8a:
 get_patches_key "reddit-rve-arsclib"
 split_editor "reddit" "reddit-arm64-v8a" "exclude" "split_config.armeabi_v7a split_config.x86_64 split_config.mdpi split_config.ldpi split_config.hdpi split_config.xxhdpi split_config.tvdpi"
+
+# Patching
 patch "reddit-arm64-v8a" "revanced-extended"
-mv ./release/reddit-arm64-v8a-revanced-extended.apk/base.apk ./reddit-arm64-v8a-revanced-extended.apk
-rm -f -d ./release/reddit-arm64-v8a-revanced-extended.apk
-mv ./reddit-arm64-v8a-revanced-extended.apk ./release/reddit-arm64-v8a-revanced-extended-arsclib.apk
+
+# Rename output
+mv "./release/reddit-arm64-v8a-revanced-extended-v${APP_VERSION}.apk" "./release/reddit-arm64-v8a-revanced-extended-arsclib-v${APP_VERSION}.apk"
