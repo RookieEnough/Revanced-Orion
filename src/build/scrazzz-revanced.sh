c6b95b26034fad6616e885f357a560509f8ac263
Sarthak Sinha <128968367+RookieEnough@users.noreply.github.com>
2025-12-26 19:59:46 +0530

    Update scrazzz-revanced.sh

--- src/build/scrazzz-revanced.sh
+++ src/build/scrazzz-revanced.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 # scrazzz build
 source ./src/build/utils.sh
+
 #################################################
 # Download requirements
 dl_gh "revanced-cli" "revanced" "latest"
 dl_gh "my-revanced-patches" "scrazzz" "prerelease"
+
 #################################################
-# Patch Soild Explorer arm64-v8a:
+# Patch Solid Explorer arm64-v8a:
 get_patches_key "Soild-Explorer"
-get_apk "pl.solidexplorer2" "solid-explorer-arm64-v8a" "solid-explorer-beta" "neatbytes/solid-explorer-beta/solid-explorer-file-manager" "arm64-v8a"
+
+# FIX: Updated slugs for APKMirror
+# Arg 3 (app name for recent uploads): "solid-explorer-file-manager"
+# Arg 4 (URL path): "neatbytes/solid-explorer/solid-explorer-file-manager"
+get_apk "pl.solidexplorer2" "solid-explorer-arm64-v8a" "solid-explorer-file-manager" "neatbytes/solid-explorer/solid-explorer-file-manager" "arm64-v8a"
+
 patch "solid-explorer-arm64-v8a" "scrazzz"
